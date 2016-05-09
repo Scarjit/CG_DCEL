7ab84da7b78fc753fdf014f3e6f38389203af313
flinnenb <ferdinand.linnenberg@stud.htwk-leipzig.de>
2016-05-09 21:05:38 +0200

    Commit 8

--- src/main/Loader.java
+++ src/main/Loader.java
@@ -1,17 +1,41 @@
 package main;
 
-import java.io.File;
+import java.io.*;
+import java.util.ArrayList;
 
 
 public class Loader {
-	File file;
+	private static ArrayList<String> file = new ArrayList<>();
+
 	public static void loadfile() {
-		GetFile();
+		readFile();
 	}
 
-	private static void GetFile() {
+	private static void readFile() {
 		System.out.println("Please insert File Name");
+		try{
+			BufferedReader inputConsole = new BufferedReader(new InputStreamReader(System.in));
+			String input = inputConsole.readLine();
+			inputConsole.close();
+			if(!input.contains(".ply")){
+				input+=".ply";
+			}
 
+			try {
+				BufferedReader fileReader = new BufferedReader(new FileReader(".\\resource\\"+input));
+				String line = fileReader.readLine();
+				while (line != null){
+					System.out.println(line);
+					file.add(line);
+					line = fileReader.readLine();
+				}
+			}catch (IOException e){
+				e.printStackTrace();
+
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
