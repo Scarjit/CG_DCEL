7b1438c240b3684555a1382f55c14757fa392782
flinnenb <ferdinand.linnenberg@stud.htwk-leipzig.de>
2016-05-09 20:11:51 +0200

    Commit 4

--- src/Main/DCELMesh.java
+++ src/Main/DCELMesh.java
@@ -1,5 +1,24 @@
 package main;
 
-public class DCELMesh {
+import java.util.ArrayList;
 
+
+public class DCELMesh {
+	private ArrayList<DCELVertex> VertexArray;
+
+	public DCELMesh(String Name){
+		System.out.println("Creating new Mesh: "+Name);
+	}
+
+	public void addVertex(DCELVertex vertex){
+		VertexArray.add(vertex);
+	}
+
+	public void removeVertex(DCELVertex vertex){
+		VertexArray.remove(vertex);
+	}
+
+	public ArrayList<DCELVertex> getVertexArray(){
+		return VertexArray;
+	}
 }
