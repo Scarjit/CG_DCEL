f038f71fb9899623ac6dbc1332cace2018a94d48
flinnenb <ferdinand.linnenberg@stud.htwk-leipzig.de>
2016-05-09 20:44:03 +0200

    Commit 6

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,6 +1,8 @@
 package main;
 public class Main {
+	public static DCELMesh mesh;
 	public static void main(String[] args) {
-
+		System.out.println("Started Application");
+		Loader.loadfile();
 	}
 }
