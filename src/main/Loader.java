c3d4efafcd23180275359a196db596d702bcf05f
flinnenb <ferdinand.linnenberg@stud.htwk-leipzig.de>
2016-05-09 20:44:50 +0200

    Commit 7

---
+++ src/main/Loader.java
@@ -0,0 +1,17 @@
+package main;
+
+import java.io.File;
+
+
+public class Loader {
+	File file;
+	public static void loadfile() {
+		GetFile();
+	}
+
+	private static void GetFile() {
+		System.out.println("Please insert File Name");
+
+	}
+
+}
