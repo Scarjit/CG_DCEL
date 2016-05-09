b47c89aeb6c8f587dacf38781479c56e779b9949
flinnenb <ferdinand.linnenberg@stud.htwk-leipzig.de>
2016-05-09 19:54:30 +0200

    Commit 1

--- src/de/scarjit/main/Main.java
+++ src/de/scarjit/main/Main.java
@@ -1,8 +1,10 @@
+package de.scarjit.main;
+
 /**
  * Created by Scarjit on 09.05.2016.
  */
 public class Main {
-	public static void Main(){
-		System.out.println("Hello World");
+	public static void main(String[] args) {
+
 	}
 }
